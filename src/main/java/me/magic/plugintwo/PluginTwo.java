6d8817643933dc8fa9c05ef78f25b2f7903955d9
BuildTools <unconfigured@null.spigotmc.org>
2022-01-16 17:54:36 +0100

    did nothing

--- src/main/java/me/magic/plugintwo/PluginTwo.java
+++ src/main/java/me/magic/plugintwo/PluginTwo.java
@@ -1,17 +1,9 @@
 package me.magic.plugintwo;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
 import me.magic.plugintwo.commands.BoomCommand;
 import me.magic.plugintwo.commands.Fly;
 import me.magic.plugintwo.commands.OpenCommand;
 import me.magic.plugintwo.commands.SmeltCountCommand;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.reflections.Reflections;
@@ -51,30 +43,6 @@ public final class PluginTwo extends JavaPlugin {
         getCommand("boom").setExecutor(new BoomCommand());
         getCommand("smeltcount").setExecutor(new SmeltCountCommand());
         getCommand("pv").setExecutor(new OpenCommand());
-
-
-        //Creating a packet listener
-        ProtocolManager manager = ProtocolLibrary.getProtocolManager();
-
-        //Listening for an Incoming packet - from the CLIENT to the SERVER
-        manager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Client.POSITION) {
-            @Override
-            public void onPacketReceiving(PacketEvent event) {
-
-                PacketContainer packet = event.getPacket();
-                Player p = event.getPlayer();
-
-                //Extracting info from the packet
-                double x = packet.getDoubles().read(0);
-                double y = packet.getDoubles().read(1);
-                double z = packet.getDoubles().read(2);;
-                boolean isOnGround = packet.getBooleans().read(0);
-
-                //p.sendMessage("INBOUND PACKET: x: " + x + " y: " + y + " z: " + z);
-                //p.sendMessage("ON GROUND? " + isOnGround);
-
-            }
-        });
 
     }
 
